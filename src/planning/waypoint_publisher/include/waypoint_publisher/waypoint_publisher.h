94e6355c07a227be2a6fdabe1a42bc939bcf45b4
ranka mori <ranka06mori@gmail.com>
2019-11-01 22:51:51 +0900

    new file:   src/planning/waypoint_publisher/CMakeLists.txt 	new file:   src/planning/waypoint_publisher/include/waypoint_publisher/waypoint_publisher.h 	new file:   src/planning/waypoint_publisher/launch/waypoint_publisher.launch 	new file:   src/planning/waypoint_publisher/package.xml 	new file:   src/planning/waypoint_publisher/src/waypoint_publisher.cpp 	new file:   src/planning/waypoint_publisher/src/waypoint_publisher_node.cpp

---
+++ src/planning/waypoint_publisher/include/waypoint_publisher/waypoint_publisher.h
@@ -0,0 +1,39 @@
+#ifndef WAYPOINT_PUBLISHER_WAYPOINT_PUBLISHER_H_INCLUDED
+#define WAYPOINT_PUBLISHER_WAYPOINT_PUBLISHER_H_INCLUDED 
+
+
+#include <ros/ros.h>
+#include <nav_msgs/Path.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Quaternion.h>
+#include <tf/tf.h>
+
+//header in Boost
+#include <boost/thread.hpp>
+#include <boost/bind.hpp>
+
+
+class WaypointPublisher
+{
+public:
+    WaypointPublisher(ros::NodeHandle nh, ros::NodeHandle pnh);
+    ~WaypointPublisher();
+private:
+    void PublishWaypoints_(void);
+    void CreatWaypoint_(void);
+    
+   
+    ros::NodeHandle nh_;
+    ros::NodeHandle pnh_;
+    ros::Time current_time_;
+
+    std::string path_topic_;
+    std::string map_frame_;
+    nav_msgs::Path wps_;
+    
+    ros::Publisher wps_pub_;
+};
+
+
+#endif //WAYPOINT_PUBLISHER_WAYPOINT_PUBLISHER_H_INCLUDED
+
